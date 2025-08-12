b4597cc465de210f1c527add01b36e37d7175dcf
Sasha <jabri.kita@gmail.com>
2025-08-13 00:48:11 +0300

    виправлені файли з дз1

---
+++ lesson_01/_selflearning03.py
@@ -0,0 +1,19 @@
+"""
+Обчислення із змінними
+Замість обчислення з фактичними значеннями, ви можете використовувати змінні.
+
+Скільки грошей ви заощадите через чотири місяці, якщо щомісяця заощаджуватимете 10 доларів?
+
+Створіть змінну monthly_savings, рівну 10, та num_months, рівну 4.
+Помножте monthly_savings на num_months і присвойте результат new_savings.
+
+"""
+# Створіть змінну monthly_savings, рівну 10, та num_months, рівну 4
+
+monthly_savings = 10
+num_months = 4
+
+# Помножте monthly_savings на num_months і збережіть результат в змінну new_savings
+new_savings = num_months * monthly_savings
+# Виведіть new_savings
+print(new_savings)
