17452bdb2d5237bb3516080e863750e69d635172
Sasha <jabri.kita@gmail.com>
2025-09-16 22:15:21 +0300

    Додано дз 8

---
+++ Homework_8/homework_08.py
@@ -0,0 +1,57 @@
+"""
+Реалізувати функцію `sum_numbers_in_list(input_list)`, яка приймає список рядків, 
+де кожен рядок містить числа, розділені комами. Функція повинна повертати список 
+із сум чисел для кожного рядка або відповідне повідомлення про помилку у 
+випадку некоректних даних.
+
+#### **Приклади виклику функції:**
+```python
+sum_numbers_in_list(["1,2,3", "4,0,6"])  # [6, 10]
+sum_numbers_in_list(["1,2,3", "asas7,8,9", "4,0,6"])  # [6, "Не можу це зробити!", 10]
+sum_numbers_in_list(["1,2,3,4", 7])  # [10, "Не можу це зробити! AttributeError"]
+sum_numbers_in_list([])  # ValueError
+sum_numbers_in_list("21")  # ValueError
+```
+"""
+
+
+def sum_numbers_in_list(string_list: list):
+    """Повертає список сум чисел зі списку строк,
+    які складаються з чисел, розділених комою."""
+    # if string_list is not list:
+    #     print('twst', string_list)
+    result = []
+    if type(string_list) != list or (string_list == []):
+        raise ValueError("Ваші дані не є списком")
+    for item in string_list:
+        try:
+            generated_list = [int(x) for x in item.split(",")]
+            result.append(sum(generated_list))
+        except AttributeError as e:
+            result.append("Не можу це зробити! AttributeError")
+        except ValueError as e:
+            result.append("Не можу це зробити!")
+
+    return result
+
+
+if __name__ == "__main__":
+    output = sum_numbers_in_list(["1,2,3", "4,0,6"])
+    print(output)
+
+    output = sum_numbers_in_list(["1,2,3", "4/0,6", "asas7,8,9"])
+    print(output)
+    output = sum_numbers_in_list(["1,2,3,4", 7])
+    print(output)
+    output = sum_numbers_in_list([])
+    print(output)
+    output = sum_numbers_in_list("21")
+    print(output)
+
+    """
+    sum_numbers_in_list(["1,2,3", "4,0,6"])  # [6, 10]
+    sum_numbers_in_list(["1,2,3", "asas7,8,9", "4,0,6"])  # [6, "Не можу це зробити!", 10]
+    sum_numbers_in_list(["1,2,3,4", 7])  # [10, "Не можу це зробити! AttributeError"]
+    sum_numbers_in_list([])  # ValueError
+    sum_numbers_in_list("21")  # ValueError
+    """
